629cfe731296f0da62f03e25d076936bc7b20a71
Ethan Kong <0_2_3@naver.com>
2024-03-28 16:24:43 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+	answer = 0
+	cnt = 0
+	for i in range(len(num_list)):
+		while num_list[i] != 1:
+			if num_list[i] % 2 == 0:
+				num_list[i] =  num_list[i]/2
+			else:
+				num_list[i] = (num_list[i]-1)/2
+			cnt += 1
+	answer = cnt           
+	return answer
