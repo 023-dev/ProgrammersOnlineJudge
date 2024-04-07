45fc6cdb0fbfac8ac1b235494348c163158975fe
Ethan Kong <0_2_3@naver.com>
2024-04-08 01:19:01 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,9 +1,3 @@
 def solution(s):
-    answer = '0'
-    tmp = s.split()
-    for i in range(len(tmp)):
-        if tmp[i] == 'Z':
-            answer += '-' +tmp[i-1]
-        else:
-            answer += '+'+tmp[i]
-    return eval(answer)
+    answer = 0
+    return answer
